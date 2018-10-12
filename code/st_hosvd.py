b9e46273dafbff939e2131f106abdd3eff6f4aff
Wouter Baert <>
2018-10-12 14:06:57 +0200

    Got basic ST-HOSVD working.

--- code/st_hosvd.py
+++ code/st_hosvd.py
@@ -1,35 +1,130 @@
 import numpy as np
 from operator import itemgetter
-from numpy import linalg
 
-def compress(data, relative_target_error=0.05):
+def compress(data, relative_target_error=0.025):
 	
 	# This function calculates the ST-HOSVD of the given 3D tensor (see https://epubs.siam.org/doi/abs/10.1137/110836067) using the mode order: 2, 0, 1
 	# data should be a numpy array
 	# relative_target_error is the desired Frobenius norm of the difference of the input data and the decompressed version of the compressed data divided by the Frobenius norm of the input data
-	# returns ((U_1, ..., U_n), S), meaning the factor matrices and the core tensor
+	# returns ([U_1, ..., U_n], S), meaning the factor matrices and the core tensor
 	
-	factor_matrices = [None]*data.ndim
-	S = data
-	sizes = data.shape
-	for mode in 2, 0, 1:
+	# Initialization
+	core_tensor = data
+	mode_order = 2, 0, 1
+	factor_matrices = [None]*core_tensor.ndim
+	"""mode_order = []
+	factor_matrices = []
+	for size in data.shape:
+		factor_matrices.append(np.identity(size))"""
+	current_sizes = list(data.shape)
+	sq_abs_target_error = (relative_target_error*np.linalg.norm(data))**2
+	sq_error_so_far = 0
+	
+	# Process modes
+	for mode_index in range(len(mode_order)):
 		
-		# Process one mode
+		mode = mode_order[mode_index]
 		
-		if mode == 0:
-			# Mode is already in front, no transposition required, convert to matrix of column vectors
-		elif mode == data.ndim:
-			# Mode is already in back, no transposition required, convert to matrix of row vectors
-		else:
-			# Transposition required, set current mode to back, convert to matrix of row vectors
+		# Transpose modes if necessary to bring current mode to front (unless current mode is at front of back already)
+		if mode != 0 and mode != data.ndim - 1:
 			transposition_order = list(range(data.ndim))
-			transposition_order[mode] = data.ndim - 1
-			transposition_order[data.ndim - 1] = mode
-			transposed = np.transpose(S, transposition_order)
+			transposition_order[mode] = 0
+			transposition_order[0] = mode
+			core_tensor = np.transpose(core_tensor, transposition_order)
 		
-		matrix = 
+		# Unfold tensor and calculate SVD
+		if mode == data.ndim - 1:
+			# Mode is already in back, convert to matrix of row vectors
+			uncompressed_matrix = np.reshape(core_tensor, (-1, current_sizes[mode]))
+			V, S, Uh = np.linalg.svd(uncompressed_matrix, full_matrices=False)
+			U = np.transpose(Uh)
+			Vh = np.transpose(V)
+		else:
+			# Mode is in front (possibly due to transposition)
+			uncompressed_matrix = np.reshape(core_tensor, (current_sizes[mode], -1))
+			U, S, Vh = np.linalg.svd(uncompressed_matrix, full_matrices=False)
+		
+		# Determine compression rank
+		sq_mode_target_error = (sq_abs_target_error - sq_error_so_far)/(data.ndim - mode_index)
+		sq_mode_error_so_far = 0
+		truncation_rank = S.shape[0]
+		for i in range(S.shape[0] - 1, -1, -1):
+			new_error = S[i]**2
+			if sq_mode_error_so_far + new_error > sq_mode_target_error:
+				# Target error was excdeeded, truncate at previous rank
+				truncation_rank = i + 1
+				break
+			else:
+				# Target error was not exceeded, add error and continue
+				sq_mode_error_so_far += new_error
+		sq_error_so_far += sq_mode_error_so_far
+		
+		# Apply compression and fold back into tensor
+		factor_matrices[mode] = U[:, :truncation_rank]
+		if mode == data.ndim - 1:
+			compressed_matrix = np.matmul(np.transpose(Vh[:truncation_rank, :]), np.diag(S[:truncation_rank]))
+			current_sizes[mode] = truncation_rank
+		else:
+			compressed_matrix = np.matmul(np.diag(S[:truncation_rank]), Vh[:truncation_rank, :])
+			if mode != 0:
+				# Will transpose later, so truncated mode is actually in front at the moment
+				current_sizes[mode] = current_sizes[0]
+				current_sizes[0] = truncation_rank
+			else:
+				current_sizes[mode] = truncation_rank
+		core_tensor = np.reshape(compressed_matrix, current_sizes)
+		
+		if mode != 0 and mode != data.ndim - 1:
+			# Transpose back to original order
+			core_tensor = np.transpose(core_tensor, transposition_order)
+	
+	return factor_matrices, core_tensor
 
-def decompress((factor_matrices, core_tensor)):
+def decompress(compressed):
 	
 	# This function converts the given Tucker decomposition to the full tensor
+	# compressed is a tuple ([U_1, ..., U_n], S), meaning the factor matrices and the core tensor
 	# returns the full tensor
+	
+	factor_matrices, core_tensor = compressed
+	
+	# Mode order is mathematically irrelevant, but may affect processing time (and maybe precision) significantly
+	data = core_tensor
+	current_sizes = list(data.shape)
+	for mode in range(len(factor_matrices)):
+		
+		# Transpose modes if necessary
+		if mode != 0 and mode != data.ndim - 1:
+			transposition_order = list(range(data.ndim))
+			transposition_order[mode] = 0
+			transposition_order[0] = mode
+			data = np.transpose(data, transposition_order)
+		
+		# Unfold tensor and transform the vectors
+		factor_matrix = factor_matrices[mode]
+		if mode == data.ndim - 1:
+			# Mode is already in back, convert to matrix of row vectors
+			compressed_matrix = np.reshape(data, (-1, current_sizes[mode]))
+			decompressed_matrix = np.matmul(compressed_matrix, np.transpose(factor_matrix))
+		else:
+			# Mode is in front (possibly due to transposition)
+			compressed_matrix = np.reshape(data, (current_sizes[mode], -1))
+			decompressed_matrix = np.matmul(factor_matrix, compressed_matrix)
+		
+		# Fold back into tensor
+		if mode == data.ndim - 1:
+			current_sizes[mode] = factor_matrix.shape[0]
+		else:
+			if mode != 0:
+				# Will transpose later, so truncated mode is actually in front at the moment
+				current_sizes[mode] = current_sizes[0]
+				current_sizes[0] = factor_matrix.shape[0]
+			else:
+				current_sizes[mode] = factor_matrix.shape[0]
+		data = np.reshape(decompressed_matrix, current_sizes)
+		
+		if mode != 0 and mode != data.ndim - 1:
+			# Transpose back to original order
+			data = np.transpose(data, transposition_order)
+	
+	return data		
--- code/test.py
+++ code/test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
+from functools import reduce
 
 import st_hosvd
 
@@ -13,17 +14,37 @@ def plot_intensity(data):
 	plt.gray()
 	plt.show()
 
-def plot_comparison(data1, data2):
+def plot_comparison(original, compressed):
 	# Shows the cumulative intensity per pixel using grayscale for two images simultaneously
-	plt.imshow(np.sum(data1, axis=2))
-	plt.imshow(np.sum(data2, axis=2))
+	plt.imshow(np.sum(original, axis=2))
 	plt.gray()
 	plt.show()
+	plt.imshow(np.sum(compressed, axis=2))
+	plt.gray()
+	plt.show()
+
+def print_difference(original, compressed):
+	diff = np.linalg.norm(original - compressed)
+	norm = np.linalg.norm(original)
+	print("Absolute error:", diff)
+	print("Initial norm:", norm)
+	print("Relative error:", abs(diff)/norm)
+
+def print_compression_rate(original, compressed):
+	original_size = original.ndim + reduce((lambda x, y: x * y), original.shape)
+	factor_matrices, core_tensor = compressed
+	compressed_size = 2*original.ndim + sum([factor_matrix.shape[0]*factor_matrix.shape[1] for factor_matrix in factor_matrices]) + reduce((lambda x, y: x * y), core_tensor.shape)
+	print("Sizes in number of floats:")
+	print("Original shape:", original.shape, "\toriginal size:", original_size)
+	print("Compressed shape:", core_tensor.shape, "\tcompressed size:", compressed_size)
+	print("Compression ratio:", compressed_size/original_size)
 
 def main():
 	data = load_indian_pines()
 	compressed = st_hosvd.compress(data)
 	decompressed = st_hosvd.decompress(compressed)
-	plot_comparison(decompressed)
+	print_difference(data, decompressed)
+	print_compression_rate(data, compressed)
+	plot_comparison(data, decompressed)
 
 main()
